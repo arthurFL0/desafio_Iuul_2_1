6e3bd9ba8a36265ad047886e4bb6ab46edfca792
Arthur <arthurlacerda206@gmail.com>
2024-11-21 11:11:40 -0300

    mudando nome arquivo

---
+++ ConversorMoedas/Conversor.cs
@@ -0,0 +1,21 @@
+﻿
+using ConversorMoedas.Interfaces;
+
+namespace ConversorMoedas
+{
+    internal class Conversor
+    {
+        private Requester requester;
+
+        public Conversor(Requester r)
+        {
+            requester = r;
+        }
+
+        public async Task<IConversao> converterValor(PedidoConversao p)
+        {
+            IConversao obj = await requester.fazerRequisicao(p);
+            return obj;
+        }
+    }
+}
